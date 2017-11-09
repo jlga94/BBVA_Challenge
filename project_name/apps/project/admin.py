8ae04be265275f2e1168c828733943e5c42b68ec
Luigi Antony Arroyo Espiritu <luartony@gmail.com>
2017-11-08 23:39:38 -0500

    luigi

--- project_name/apps/project/admin.py
+++ project_name/apps/project/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
--- project_name/apps/project/models.py
+++ project_name/apps/project/models.py
@@ -1,1 +1,24 @@
 # Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from project_name.apps.core.utils.fields import BaseModel
+
+class Project(BaseModel):
+# Create your models here.
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    periods = models.IntegerField()
+    contractor = models.CharField(max_length=100)
+	resident = models.CharField(max_length=100)
+	supervisor = models.CharField(max_length=100)
+	execution_time = models.CharField(max_length=100)
+	start_date = models.DateTimeField()
+	end_date = models.DateTimeField()
+    
+    class Meta:
+        verbose_name = _("Project")
+        verbose_name_plural = _("Project")
+        unique_together = ['name']
+
+    def __str__(self):
+        return self.name
