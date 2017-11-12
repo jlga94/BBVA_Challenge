b8491c945616a0e5957e6773abbd64475f0729c7
Luigi Antony Arroyo Espiritu <luartony@gmail.com>
2017-11-12 11:25:52 -0500

    luigi

--- Iniciar servidor.bat
+++ Iniciar servidor.bat
@@ -1,3 +1,4 @@
 cd "c://app_evm"
-C:\app_evm>python manage.py runserver 0.0.0.0:80 --settings=project_name.confi
+python manage.py runserver 0.0.0.0:80 --settings=project_name.confi
 g.settings.local
+pause
