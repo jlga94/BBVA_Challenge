6c1c82c04798de7bee1c731d532b779b1bc4de52
Luigi Arroyo Espiritu <luartony@gmail.com>
2018-01-29 18:54:39 -0300

    Actilizacion Proyecto

--- project_name/apps/project/urls.py
+++ project_name/apps/project/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     url(r'^project/$', views.ProjectView.as_view(), name="project"),
     url(r'^project/save/dg$', views.save_datos_generales, name="save_datos_generales"),
     url(r'^project-list/$', views.ProjectListView.as_view(), name="project-list"),
+    url(r'^lista-pagos/$', views.PagosListView.as_view(), name="pagos-list"),
     url(r'^project-detail/(?P<pk>[0-9]+)/$', views.ProjectDetailView.as_view(), name='project-detail'), 
     url(r'^project-list/(?P<pk>[0-9]+)/results/$', views.ProjectResultsView.as_view(), name="project-result"),
+    url(r'^vista-pagos/$', views.VistaPagosListView.as_view(), name="vista-pagos"),   
+    url(r'^detalle-pago/$', views.DetallePagosListView.as_view(), name="detalle_pago"),   
 ]
--- project_name/apps/project/views.py
+++ project_name/apps/project/views.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView,DetailView
 from . import models
 from django.http import JsonResponse
 from .forms import ProyectForm
-
+import requests 
+#import requests
 
 class ProjectView(TemplateView):
     template_name = 'themes/pages/project/project.html'
@@ -33,7 +34,58 @@ class ProjectListView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["project"] = models.Project.objects.all() 
         return context
-        
+
+class VistaPagosListView(TemplateView):
+    template_name = 'themes/pages/project/pagos.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()        
+        return super().render_to_response(self.get_context_data())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class PagosListView(TemplateView):
+    model = models.Project
+    template_name = 'themes/pages/project/project_list.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().render_to_response(self.get_context_data())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project"] = models.Project.objects.all() 
+
+        url = 'http://10.17.182.204:7000/pagos/' 
+        params = {'cd_id_empresa': 1}
+
+        r = requests.get(url, params=params)
+        pagos = r.json()
+        context["pagos"] = pagos
+        return context
+
+class DetallePagosListView(TemplateView):
+    template_name = 'themes/pages/project/detalle_Pago.html'
+
+    {
+        "T009_Paypal":{
+        "num_tarjeta":"1" ,
+        "cod_csv":"1",
+        "tit_apellidos":"Gonzalez Prada",
+        "tit_nombres":"Victor",
+        "tit_correo":"victor.gonz@gmail.com",
+        }
+    }
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()        
+        return super().render_to_response(self.get_context_data())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 class ProjectDetailView(DetailView):
     """docstring for ProjectDetailView"""
     model = models.Project
@@ -48,6 +100,8 @@ class ProjectDetailView(DetailView):
         context["form"] = form
 
         return context
+
+
 
 def save_datos_generales(request): 
     # username = request.GET
